28a92d5e988412a0d4ad4a5bf2e2a9fc9569ae1f
gandalfs <franklinortiz93@hotmail.com>
2022-04-10 17:17:20 -0500

    Se crea clase para locations

---
+++ app/src/main/java/com/aprendizaje/rickandmorty/actividades/Locations.java
@@ -0,0 +1,16 @@
+package com.aprendizaje.rickandmorty.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aprendizaje.rickandmorty.R;
+
+public class Locations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locations);
+    }
+}
